95b04662d68e441e65c74b5cf241121691ef7cb7
Patrick Michl <huanzodev@gmail.com>
2020-12-02 09:05:46 +0100

    remove debug print

--- day_02/second.rb
+++ day_02/second.rb
@@ -4,6 +4,5 @@ a = input.map do |pwl|
   matches = pwl.match /(?<start>\d+)-(?<end>\d+) (?<char>\w): (?<pw>\w+)/
   matches[:pw].chars.values_at(matches[:start].to_i-1, matches[:end].to_i-1).count(matches[:char])
 end
-puts a.inspect
 puts a.count(1)
 
